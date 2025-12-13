aa640edc60b77bc8429f8d7137042b5a4c36a221
NguyenDucTri <tri11122006@gmail.com>
2025-12-14 06:49:07 +0700

    Implement Scoop installation script
    Add commands to install Scoop in PowerShell.

--- install_scoop.ps1
+++ install_scoop.ps1
@@ -1,1 +1,5 @@
-
+start powershell -wait -argumentlist @"
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+exit
+"@
