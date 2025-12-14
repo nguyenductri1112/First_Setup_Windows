f3d77568beda4d4d09a3f699b908df25a4ccdfb8
NguyenDucTri <tri11122006@gmail.com>
2025-12-14 07:02:16 +0700

    Update install_script.ps1 to download scripts from GitHub

--- install_script.ps1
+++ install_script.ps1
@@ -1,1 +1,2 @@
-iwr " " - outfile "C:\atool.ps1" 
+iwr "https://github.com/nguyenductri1112/First_Setup_Windows/releases/download/auto/a_tool.ps1" - outfile "C:\atool.ps1" 
+iwr "https://github.com/nguyenductri1112/First_Setup_Windows/releases/download/auto/install_scoop.ps1" -outfile "C:\installscoop.ps1"
